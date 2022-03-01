e9ab2b78764a958c692f0ca8b5615efd6c70019a
Kimjongho75 <100732964+Kimjongho75@users.noreply.github.com>
2022-03-02 08:09:47 +0900

    Add files via upload
    new

---
+++ Form1.Designer.vb
@@ -0,0 +1,330 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form은 Dispose를 재정의하여 구성 요소 목록을 정리합니다.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows Form 디자이너에 필요합니다.
+    Private components As System.ComponentModel.IContainer
+
+    '참고: 다음 프로시저는 Windows Form 디자이너에 필요합니다.
+    '수정하려면 Windows Form 디자이너를 사용하십시오.  
+    '코드 편집기에서는 수정하지 마세요.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.CtxMenu = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripMenuItem2 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem3 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripMenuItem4 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.txtProxyport = New System.Windows.Forms.TextBox()
+        Me.txtProxyserver = New System.Windows.Forms.TextBox()
+        Me.txtIPdetect = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.CtxMenu.SuspendLayout()
+        Me.Panel1.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SplitContainer1.Panel1.SuspendLayout()
+        Me.SplitContainer1.Panel2.SuspendLayout()
+        Me.SplitContainer1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.ContextMenuStrip = Me.CtxMenu
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        '
+        'CtxMenu
+        '
+        Me.CtxMenu.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1, Me.ToolStripSeparator1, Me.ToolStripMenuItem2, Me.ToolStripMenuItem3, Me.ToolStripSeparator2, Me.ToolStripMenuItem4})
+        Me.CtxMenu.Name = "CtxMenu"
+        Me.CtxMenu.Size = New System.Drawing.Size(171, 112)
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(170, 24)
+        Me.ToolStripMenuItem1.Text = "Show"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(167, 6)
+        '
+        'ToolStripMenuItem2
+        '
+        Me.ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        Me.ToolStripMenuItem2.Size = New System.Drawing.Size(170, 24)
+        Me.ToolStripMenuItem2.Text = "Proxy Enabled"
+        '
+        'ToolStripMenuItem3
+        '
+        Me.ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        Me.ToolStripMenuItem3.Size = New System.Drawing.Size(170, 24)
+        Me.ToolStripMenuItem3.Text = "Proxy Disabled"
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(167, 6)
+        '
+        'ToolStripMenuItem4
+        '
+        Me.ToolStripMenuItem4.Name = "ToolStripMenuItem4"
+        Me.ToolStripMenuItem4.Size = New System.Drawing.Size(170, 24)
+        Me.ToolStripMenuItem4.Text = "App Exit"
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.DodgerBlue
+        Me.Panel1.Controls.Add(Me.GroupBox1)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(493, 161)
+        Me.Panel1.TabIndex = 4
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.txtProxyport)
+        Me.GroupBox1.Controls.Add(Me.txtProxyserver)
+        Me.GroupBox1.Controls.Add(Me.txtIPdetect)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.ForeColor = System.Drawing.Color.White
+        Me.GroupBox1.Location = New System.Drawing.Point(18, 14)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(358, 130)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Setting"
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.SkyBlue
+        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.DeepSkyBlue
+        Me.Button2.FlatAppearance.BorderSize = 2
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("굴림", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button2.ForeColor = System.Drawing.Color.White
+        Me.Button2.Location = New System.Drawing.Point(9, 95)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(340, 26)
+        Me.Button2.TabIndex = 6
+        Me.Button2.Text = "Save Setting"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'txtProxyport
+        '
+        Me.txtProxyport.Location = New System.Drawing.Point(140, 71)
+        Me.txtProxyport.Name = "txtProxyport"
+        Me.txtProxyport.Size = New System.Drawing.Size(209, 21)
+        Me.txtProxyport.TabIndex = 5
+        '
+        'txtProxyserver
+        '
+        Me.txtProxyserver.Location = New System.Drawing.Point(140, 44)
+        Me.txtProxyserver.Name = "txtProxyserver"
+        Me.txtProxyserver.Size = New System.Drawing.Size(209, 21)
+        Me.txtProxyserver.TabIndex = 4
+        '
+        'txtIPdetect
+        '
+        Me.txtIPdetect.Location = New System.Drawing.Point(140, 18)
+        Me.txtIPdetect.Name = "txtIPdetect"
+        Me.txtIPdetect.Size = New System.Drawing.Size(209, 21)
+        Me.txtIPdetect.TabIndex = 3
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.SkyBlue
+        Me.Label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label3.Location = New System.Drawing.Point(10, 71)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(124, 21)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Proxy Port"
+        Me.Label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.SkyBlue
+        Me.Label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label2.Location = New System.Drawing.Point(10, 44)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(124, 21)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Proxy Server"
+        Me.Label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.SkyBlue
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.Location = New System.Drawing.Point(10, 18)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(124, 21)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "IP Detection"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.SkyBlue
+        Me.Button1.FlatAppearance.BorderColor = System.Drawing.Color.DeepSkyBlue
+        Me.Button1.FlatAppearance.BorderSize = 2
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("굴림", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button1.ForeColor = System.Drawing.Color.White
+        Me.Button1.Location = New System.Drawing.Point(382, 20)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(97, 124)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Hide"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button3
+        '
+        Me.Button3.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button3.FlatAppearance.BorderSize = 0
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("굴림", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.White
+        Me.Button3.Location = New System.Drawing.Point(458, 5)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(30, 23)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "X"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Label4.Font = New System.Drawing.Font("Monospac821 BT", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.White
+        Me.Label4.Location = New System.Drawing.Point(5, 5)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(263, 22)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Automatic proxy changer"
+        '
+        'SplitContainer1
+        '
+        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.SplitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1
+        Me.SplitContainer1.Location = New System.Drawing.Point(3, 3)
+        Me.SplitContainer1.Name = "SplitContainer1"
+        Me.SplitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal
+        '
+        'SplitContainer1.Panel1
+        '
+        Me.SplitContainer1.Panel1.BackColor = System.Drawing.Color.DodgerBlue
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button4)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Button3)
+        Me.SplitContainer1.Panel1.Controls.Add(Me.Label4)
+        Me.SplitContainer1.Panel1.Padding = New System.Windows.Forms.Padding(5)
+        '
+        'SplitContainer1.Panel2
+        '
+        Me.SplitContainer1.Panel2.Controls.Add(Me.Panel1)
+        Me.SplitContainer1.Size = New System.Drawing.Size(493, 195)
+        Me.SplitContainer1.SplitterDistance = 33
+        Me.SplitContainer1.SplitterWidth = 1
+        Me.SplitContainer1.TabIndex = 5
+        '
+        'Button4
+        '
+        Me.Button4.Dock = System.Windows.Forms.DockStyle.Right
+        Me.Button4.FlatAppearance.BorderSize = 0
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("굴림", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(129, Byte))
+        Me.Button4.ForeColor = System.Drawing.Color.White
+        Me.Button4.Location = New System.Drawing.Point(428, 5)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(30, 23)
+        Me.Button4.TabIndex = 8
+        Me.Button4.Text = "_"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Gray
+        Me.ClientSize = New System.Drawing.Size(499, 201)
+        Me.Controls.Add(Me.SplitContainer1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Padding = New System.Windows.Forms.Padding(3)
+        Me.Text = "Proxy Auto Change"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        Me.CtxMenu.ResumeLayout(False)
+        Me.Panel1.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.SplitContainer1.Panel1.ResumeLayout(False)
+        Me.SplitContainer1.Panel1.PerformLayout()
+        Me.SplitContainer1.Panel2.ResumeLayout(False)
+        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.SplitContainer1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+    Friend WithEvents CtxMenu As ContextMenuStrip
+    Friend WithEvents ToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents ToolStripMenuItem4 As ToolStripMenuItem
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Label4 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents txtProxyport As TextBox
+    Friend WithEvents txtProxyserver As TextBox
+    Friend WithEvents txtIPdetect As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents SplitContainer1 As SplitContainer
+    Friend WithEvents Button4 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,165 @@
+﻿Imports System.Net.NetworkInformation
+
+Public Class Form1
+
+    Private mousePoint As Point
+    Private ProxyTag As Boolean = False
+    Private showBalloon As Boolean = True
+
+    Private INIfile As String = AppDomain.CurrentDomain.BaseDirectory & "ProxyAutoChange.ini"
+    Private Sub EnabledProxy()
+        Dim clsProxy As New IEProxy
+        Dim proxyConnect As String = ""
+        Dim proxyServer As String = ReadIni(INIfile, "SETTING", "ProxyServer", "")
+        Dim proxyPort As String = ReadIni(INIfile, "SETTING", "ProxyPort", "")
+
+        If proxyServer = "" Or proxyPort = "" Then
+            NotifyIcon1.ShowBalloonTip(2000, "Failed", "It is not setting. Use after setting.", ToolTipIcon.Error)
+            showMe()
+        End If
+
+        proxyConnect = String.Format("{0}:{1}", proxyServer, proxyPort)
+
+        If clsProxy.SetProxy(proxyConnect) Then
+            'MessageBox.Show("Proxy successfully enabled.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            If Not (ProxyTag) Or showBalloon Then NotifyIcon1.ShowBalloonTip(1000, "Success", "Proxy successfully enabled.", ToolTipIcon.Info)
+            ProxyTag = True
+            showBalloon = False
+            NotifyIcon1.Text = "Proxy enabled."
+            NotifyIcon1.Icon = My.Resources.Sunny
+            CtxMenu.Items(2).Enabled = False
+            CtxMenu.Items(3).Enabled = True
+
+        Else
+            'MessageBox.Show("Error enabling proxy.", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            NotifyIcon1.ShowBalloonTip(2000, "Failed", "Error enabling proxy.", ToolTipIcon.Error)
+        End If
+    End Sub
+
+    Private Sub DisabledProxy()
+        Dim clsProxy As New IEProxy
+
+        If clsProxy.DisableProxy Then
+            'MessageBox.Show("Proxy successfully disabled.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            If ProxyTag Or showBalloon Then NotifyIcon1.ShowBalloonTip(1000, "Success", "Proxy successfully disabled.", ToolTipIcon.Info)
+            ProxyTag = False
+            showBalloon = False
+            NotifyIcon1.Text = "Proxy disabled."
+            NotifyIcon1.Icon = My.Resources.Moon
+            CtxMenu.Items(2).Enabled = True
+            CtxMenu.Items(3).Enabled = False
+        Else
+            'MessageBox.Show("Error disabling proxy.", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+            NotifyIcon1.ShowBalloonTip(2000, "Failed", "Error disabling proxy.", ToolTipIcon.Error)
+        End If
+    End Sub
+
+    Private Sub ChangeNetwork()
+        Dim currentIP As String = ReadIni(INIfile, "SETTING", "IPDetection", "")
+        Dim isCurrentIP As Boolean = False
+        Dim strHostName = System.Net.Dns.GetHostName()
+
+        For Each ilist As Net.IPAddress In Net.Dns.GetHostEntry(strHostName).AddressList.Where(Function(a As Net.IPAddress) Not a.IsIPv6LinkLocal AndAlso Not a.IsIPv6Multicast AndAlso Not a.IsIPv6SiteLocal)
+            If ilist.ToString.StartsWith(currentIP) Then
+                isCurrentIP = True
+                Exit For
+            End If
+        Next
+
+        If isCurrentIP Then
+            EnabledProxy()
+        Else
+            DisabledProxy()
+        End If
+    End Sub
+
+    Private Sub showMe()
+        Me.txtIPdetect.Text = ReadIni(INIfile, "SETTING", "IPDetection", "")
+        Me.txtProxyserver.Text = ReadIni(INIfile, "SETTING", "ProxyServer", "")
+        Me.txtProxyport.Text = ReadIni(INIfile, "SETTING", "ProxyPort", "")
+
+        Me.Show()   '.Visible = True
+        Me.ShowInTaskbar = True  '현재 프로그램을 테스크 바에 표시하게 한다.    
+        Me.WindowState = FormWindowState.Normal  ' 폼을 윈도 상태를 normal
+        NotifyIcon1.Visible = False '트레이의 아이콘을 보이지 않게 한다. 
+
+    End Sub
+
+    Private Sub hideMe()
+
+        Me.Hide()                   '폼을 보이지 않게 한다. alt+tab 시 보이지 않는다.
+        NotifyIcon1.Visible = True   '트레이의 아이콘을 보이게 한다.
+        Me.NotifyIcon1.Text = "ProxyAutoChagne is Running!"
+
+    End Sub
+
+    Private Sub CtxMenu_Click(ByVal sender As Object, ByVal e As System.Windows.Forms.ToolStripItemClickedEventArgs) Handles CtxMenu.ItemClicked
+        Select Case e.ClickedItem.ToString()
+            Case "Show"
+                showMe()
+            Case "Proxy Enabled"
+                EnabledProxy()
+            Case "Proxy Disabled"
+                DisabledProxy()
+            Case "App Exit"
+                Application.Exit()
+        End Select
+
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+
+        ' 네트워크 변경(ip 변경) 감지 event 등록
+        AddHandler NetworkChange.NetworkAddressChanged, AddressOf ChangeNetwork
+
+        ' form 숨기기
+        hideMe()
+
+        ' 프로그램 실행 시 처음 실행하기
+        ChangeNetwork()
+
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        ' 트레이 아이콘 더블클릭으로 form 표시
+        showMe()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        hideMe()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        WriteIni(INIfile, "SETTING", "IPDetection", txtIPdetect.Text)
+        WriteIni(INIfile, "SETTING", "ProxyServer", txtProxyserver.Text)
+        WriteIni(INIfile, "SETTING", "ProxyPort", txtProxyport.Text)
+
+        hideMe()
+        EnabledProxy()
+    End Sub
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown, SplitContainer1.Panel1.MouseDown, Label4.MouseDown
+        If (e.Button And MouseButtons.Left) = MouseButtons.Left Then
+            mousePoint = New Point(e.X, e.Y)
+        End If
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove, SplitContainer1.Panel1.MouseMove, Label4.MouseMove
+        If (e.Button And MouseButtons.Left) = MouseButtons.Left Then
+            Me.Location = New Point(Me.Location.X + (e.X - mousePoint.X), Me.Location.Y + (e.Y - mousePoint.Y))
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        hideMe()
+    End Sub
+
+    Private Function ConfirmExit() As Integer
+        Return MsgBox("프로그램을 종료하시겠습니까?", MsgBoxStyle.OkCancel, "종료 확인")
+    End Function
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        If ConfirmExit() = 1 Then
+            Application.Exit()
+        End If
+    End Sub
+End Class
